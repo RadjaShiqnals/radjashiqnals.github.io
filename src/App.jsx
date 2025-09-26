4d57398c84204955cc6681980ee42f95e6d9d617
RadjaShiqnals <radjagenta@gmail.com>
2025-09-26 16:34:46 +0700

    Initialize project with React, Tailwind, and config files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-slate-900 h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-white">
+        My Portfolio is Running!
+      </h1>
+    </div>
+  )
+}
+
+export default App
